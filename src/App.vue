5aab0443f6737ae15f1fc3443bdeda74d7431144
Hyun-h <hyeonahong@gmail.com>
2021-11-07 17:30:48 +0900

    add it title

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <v-app>
     <v-app-bar app title="모여봐요, 청년의 숲">
-      <v-spacer/>
-      <v-app-bar-title class="logo">
-        모여봐요, 청년의 숲
-      </v-app-bar-title>
-      <v-spacer/>
+      <v-spacer />
+      <v-app-bar-title class="logo"> test </v-app-bar-title>
+      <v-spacer />
     </v-app-bar>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
-      <!-- <header>
+    <!-- <header>
         <div class="nav_container">
           <div class="logo text-center">어서와요, 청년의 숲</div>
         </div>
@@ -30,25 +28,25 @@
         </ul>
       </header> -->
 
-      <v-bottom-navigation app v-model="value">
-        <v-btn value="recent" to="/">
-          <span>홈</span>
-          <v-icon>mdi-home</v-icon>
-        </v-btn>
-        <v-btn value="favorites" to="/review">
-          <span>후기</span>
-          <v-icon>mdi-message-draw</v-icon>
-        </v-btn>
-        <v-btn value="nearby" to="/community">
-          <span>커뮤니티</span>
-          <v-icon>mdi-account-group</v-icon>
-        </v-btn>
-        <v-btn value="nearby" to="/mypage">
-          <span>내공간</span>
-          <v-icon>mdi-cog</v-icon>
-        </v-btn>
-      </v-bottom-navigation>
-<!--      <v-footer app>© 2021. Hyeona Co. all rights reserved.</v-footer>-->
+    <v-bottom-navigation app v-model="value">
+      <v-btn value="recent" to="/">
+        <span>홈</span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-btn value="favorites" to="/review">
+        <span>후기</span>
+        <v-icon>mdi-message-draw</v-icon>
+      </v-btn>
+      <v-btn value="nearby" to="/community">
+        <span>커뮤니티</span>
+        <v-icon>mdi-account-group</v-icon>
+      </v-btn>
+      <v-btn value="nearby" to="/mypage">
+        <span>내공간</span>
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+    <!--      <v-footer app>© 2021. Hyeona Co. all rights reserved.</v-footer>-->
   </v-app>
 </template>
 <script>
@@ -67,5 +65,4 @@ export default {
   },
 };
 </script>
-<style>
-</style>
+<style></style>
