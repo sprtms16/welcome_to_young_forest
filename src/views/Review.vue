6d64c1048c691e79f7ddc8842f649a7d8d871400
Hyun-h <hyeonahong@gmail.com>
2021-11-11 10:06:13 +0900

    이름 수정

--- src/views/Review.vue
+++ src/views/Review.vue
@@ -35,7 +35,7 @@ export default {
   methods: {
     detail(index) {
       this.$router.push({
-        name: "reviewDetail",
+        name: "Detail",
         params: {
           contentId: index,
         },
